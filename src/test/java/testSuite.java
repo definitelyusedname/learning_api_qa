6f5021b3fe935b4d8b897dd1b81f3d95472641f6
definitelyusedname <definitelyusedmail@gmail.com>
2022-10-25 00:28:01 +0600

    added getJsonHomeworkTest

--- src/test/java/testSuite.java
+++ src/test/java/testSuite.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -11,4 +12,15 @@ public class testSuite {
                 .andReturn();
         response.prettyPrint();
     }
+
+    @Test
+    public void getJsonHomeworkTest() {
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        String answer = response.get("messages[1].message");
+        System.out.println(answer);
+    }
+
 }
