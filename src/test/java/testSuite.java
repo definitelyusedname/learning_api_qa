1960da58837f38b9e0845bef08ed9934dab61cd9
definitelyusedname <definitelyusedmail@gmail.com>
2022-11-04 20:10:44 +0600

    added getLongRedirectEndTest

--- src/test/java/testSuite.java
+++ src/test/java/testSuite.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
@@ -21,6 +23,19 @@ public class testSuite {
                 .jsonPath();
         String answer = response.get("messages[1].message");
         System.out.println(answer);
+    }
+
+    @Test
+    public void getLongRedirectEndTest() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(true)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        System.out.println(response.getHeader("X-Host"));
+
     }
 
 }
