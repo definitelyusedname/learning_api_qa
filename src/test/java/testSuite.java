f4e00ccce7d9d58a159ac3bc71c7779368f571a8
definitelyusedname <definitelyusedmail@gmail.com>
2022-11-04 21:06:34 +0600

    added getLongRedirectURLsTest

--- src/test/java/testSuite.java
+++ src/test/java/testSuite.java
@@ -35,7 +35,31 @@ public class testSuite {
                 .get("https://playground.learnqa.ru/api/long_redirect")
                 .andReturn();
         System.out.println(response.getHeader("X-Host"));
+    }
 
+    @Test
+    public void getLongRedirectURLsTest() {
+
+        int responseStatus = 0;
+        int redirectCount = 0;
+        String location = "https://playground.learnqa.ru/api/long_redirect";
+        while (responseStatus != 200) {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(location)
+                    .andReturn();
+
+            responseStatus = response.getStatusCode();
+            if(responseStatus != 200) {
+                location = response.getHeader("location");
+                System.out.println("Redirected to: " + location);
+                redirectCount++;
+            }
+        }
+        System.out.println("Redirect count = " + redirectCount);
     }
 
 }
